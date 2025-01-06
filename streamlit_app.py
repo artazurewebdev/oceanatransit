7e263b42378ed8e5e83a438ba113307df30fbf0c
artazurewebdev <mmais2342@gmail.com>
2025-01-06 18:21:40 +0000

    OceanaTransit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("HyperloopOne Portal")
+
+
+with st.expander("Credits"):
+    st.write("This website was made by Max Shuang using Streamlit on Python Code")
+    st.write("This website is purely hypothetical, and is not affiliated with Virgin Group")
+    st.write("Do not make any purchases, or share sensitive information here.")
+    st.write("The redirection to HyperloopOne's transit map was made using Tennessine.uk's Metro Building by the same maker of this website")
+    st.write("This is an official website of Team Oceana for the Future City Project of 2025")
+
+url = 'https://tennessine.co.uk/metro/70f7f437184cfe3'
+
+st.markdown(f'''
+<a href={url}><button style="background-color:Gray;">See HyperloopOne Transit System</button></a>
+''',
+unsafe_allow_html=True)
